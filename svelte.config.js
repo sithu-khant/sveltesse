588113a0cd80690775e301296caf33c6f45992c4
Sithu Khant <122719409+heysithu@users.noreply.github.com>
2026-04-27 15:11:40 +0630

    chore: add remote functions config and path alias

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,21 @@ import adapter from '@sveltejs/adapter-cloudflare';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   compilerOptions: {
+    experimental: {
+      async: true
+    },
     // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
     runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
   },
-  kit: { adapter: adapter() }
+  kit: {
+    experimental: {
+      remoteFunctions: true
+    },
+    adapter: adapter(),
+    alias: {
+      '~': 'src'
+    }
+  }
 };
 
 export default config;
